774122ee967f340a44ff422b6e9eeb46234e656f
YvesLongchamp <longchampyves84@gmail.com>
2016-05-05 18:13:30 +0200

    Changing the PHP, we can get the data now.

--- mySQL.php
+++ mySQL.php
@@ -1,20 +1,41 @@
-
-mysqli.php
 <?php
-    $host_name  = "db624774209.db.1and1.com";
-    $database   = "db624774209";
-    $user_name  = "dbo624774209";
-    $password   = "loliBanane72";
-
-
-    $connect = mysqli_connect($host_name, $user_name, $password, $database);
-    
-    if(mysqli_connect_errno())
-    {
-    echo '<p>La connexion n a pas été réalisée : '.mysqli_connect_error().'</p>';
+/*
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
     }
-    else
-    {
-    echo '<p>La connexion a été réalisée.</p>';
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
     }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+} 
+*/
+$servername = "db624774209.db.1and1.com";
+$database   = "db624774209";
+$username = "dbo624774209";
+$password = "This is not my password D:";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM users"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
 ?>
