e7e26ac266f5a0fb9f48ed71d494a95544cb575d
YvesLongchamp <longchampyves84@gmail.com>
2016-05-12 13:53:21 +0200

    Adding a few changes

--- PHP/checkingSQL.php
+++ PHP/checkingSQL.php
@@ -10,7 +10,7 @@
     $servername = "db624774209.db.1and1.com";
     $database   = "db624774209";
     $username = "dbo624774209";
-    $passwordDB = "loliBanane72";
+    $passwordDB = "not my password D:";
 try {
     
     // connexion
